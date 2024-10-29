1974d29d9686672075af874e00ac3815ed38f10e
Sayantan Singha <sayantan.singha@webskitters.in>
2024-10-29 11:35:19 +0530

    ts config

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,14 @@
+import { inject } from '@angular/core';
 import { CanActivateFn, CanDeactivateFn } from '@angular/router';
+import { AuthService } from '@services';
 
 export const loginGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
   return true;
 };
 
 export const nonLoginGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
   return true;
 }
 
