c82e5496ade4e593ce4027715c2291f862f04b90
Sayantan Singha <sayantan.singha@webskitters.in>
2024-10-30 19:57:43 +0530

    alert service

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -28,4 +28,41 @@ export class AlertService {
 
     toast.fire();
   }
+  
+  /**
+   * 
+   * @param title can be a string or html in string format [`<strong>HTML <u>example</u></strong>`]
+   * @param msg can be a string or html in string format [`<strong>HTML <u>example</u></strong>`]
+   * @param type will set modal type(icon) ['success' | 'warning' | 'error' | 'info' | 'question']
+   * @param showCloseButton will take boolean value and show close button at top right of modal
+   * @param showCancelButton will take boolean value and show cancel button
+   * @param confirmButtonText set text to confirm button, can be a string or html in string format. default is 'Confirm'
+   * @param cancelButtonText set text to Cancel button, can be a string or html in string format. default is 'Cancel'
+   */
+  public modalAlert(title:string='', msg:string='', type:alertType, showCloseButton:boolean=true, showCancelButton:boolean=true, confirmButtonText:string='Confirm', cancelButtonText:string='Cancel') {
+    return Swal.fire({
+      title: title,
+      icon: type,
+      html: msg,
+      showCloseButton: showCloseButton,
+      showCancelButton: showCancelButton,
+      focusConfirm: false,
+      confirmButtonText: confirmButtonText,
+      //`<i class="fa fa-thumbs-up"></i> Great!`,
+      confirmButtonAriaLabel: "Confirm",
+      cancelButtonText: cancelButtonText,
+      //`<i class="fa fa-thumbs-down"></i>`,
+      cancelButtonAriaLabel: "Cancel"
+    });
+
+    // return Swal.fire({
+    //   title: title,
+    //   text: msg,
+    //   icon: type,
+    //   showCancelButton: showCancelButton,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: confirmButtonText
+    // })
+  }
 }
