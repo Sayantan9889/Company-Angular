a9b88280f13547ff9f129c1cc19f09c25fc5d2c2
Sayantan9889 <sayantan.silda98@gmail.com>
2024-11-05 00:34:13 +0530

    Changes

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
